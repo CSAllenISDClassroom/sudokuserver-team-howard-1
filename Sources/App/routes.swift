3d18fba5ad0039ec8f85c2c63e4027672a0aff01
jeremie.hews <jeremie.hews@student.allenisd.org>
2021-09-22 13:30:45 -0500

    added filesxsxc

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,28 +1,34 @@
 import Vapor
+var runningGames = [Int: sudokuBoard] ()
 
-struct Background : Content {
-    let Action: String
-    let Payload: String
-    let Response: String
-    let StatusCode: String
-}
+
+
 ///////////////////
-func create(req: Request) throws -> HTTPStatus {
+/*func create(req: Request) throws -> HTTPStatus {
     let background = try req.content.decode(Background.self)
     print(background)
     return HTTPStatus.ok
-}       
+} */      
 //////////////////
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
-    app.get("games",":id","cells") { req -> String in
-        return "haha ur so smart"
-    }
-    app.post("games") { req -> String in
-        return "test" 
+    
+    app.post("games") {req -> [String:String] in
+        let partialBoard = sudokuBoard(boardString: makeBoard())
+        let gameID = GameID.createID(runningGames:runningGames)
+        runningGames[gameID] = partialBoard
         
+        return ["id": String(gameID)]
+        
+        }
+    
+    app.get("games",":id","cells") { req -> String in
+        let id = Int(req.parameters.get("id")!)!
+        let partialBoard = runningGames[id]!
+        let response = partialBoard()
+        return response
     }
     app.put("games",":id","cells",":boxIndex",":cellIndex") { req -> String in
         return "test"
@@ -32,22 +38,3 @@ func routes(_ app: Application) throws {
 }
 
 
-
-/*
-POST /games
-  * Action: Creates a new game and associated board
-  * Payload: None
-  * Response: Id uniquely identifying a game
-  * Status code: 201 Created
-GET /games/<id>/cells
-              * Action: None
-              * Payload: None
-              * Response: cells
-              * Status code: 200 OK
-PUT /games/<id>/cells/<boxIndex>/<cellIndex>
-* Action: Place specified value at in game at boxIndex, cellIndex
-* Payload: value (null for removing value)
- Response: Nothing
- Status: 204 No Content
-                                                                                   */
- 
