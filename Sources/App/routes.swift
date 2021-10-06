e3460e54e3993bc91f0130165acdceaeb303f346
Michael McPartland <michael.mcpartland@student.allenisd.org>
2021-10-06 14:17:59 -0500

    fixed a duplication problem

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -46,20 +46,6 @@ func routes(_ app: Application) throws {
     
     ///////////////////////////////////////////////// given specific board id box and cell allows you to change the value inside of the box
     app.put("games",":id","cells",":boxIndex",":cellIndex") { req -> String in
-        guard let id = req.parameters.get("id", as: Int.self),
-              let boxIndex = req.parameters.get("boxIndex", as: Int.self),
-              let cellIndex = req.parameters.get("cellIndex", as: Int.self) else {
-            throw Abort(.badRequest)
-        }
-        // let id = Int(req.parameters.get("id")!)!
-        // let boxIndex = Int(req.parameters.get("boxIndex")!)!
-        // let cellIndex = Int(req.parameters.get("cellIndex")!)!
-         let partialBoard = runningGames[id]!
-         let num = Int(req.body.string!)!
-         runningGames[id] = sudokuBoard(boardString: alterCell(boardString: partialBoard.boardString, num: num, boxIndex: boxIndex, cellIndex: cellIndex))
-         return alterCell(boardString: partialBoard.boardString, num: num, boxIndex: boxIndex, cellIndex: cellIndex)
-    }     
-
         guard let id = req.parameters.get("id", as: Int.self) else{
             throw Abort(.badRequest)
         }
