aa2e40f35844ea592e4e8e49528860a7d4d0906f
Michael McPartland <michael.mcpartland@student.allenisd.org>
2021-10-20 14:57:30 -0500

    Converted to dynamic library

--- Sources/App/GameID.swift
+++
@@ -1,9 +0,0 @@
-//////////////////////////////////////////////////creates the game IDs which will be associated with the boards
-class GameID{
-    init(){
-    }
-    //////////Generates a random number utilized to represent the GameID
-    public static func createID(runningGames: [Int:sudokuBoard]) -> Int{
-        return Int.random(in: 1 ... 10000)
-    }
-}
